b6cb1a9a20d2bbf78e8ae859c1504d0134c6e41c
Samuel Cornet <samuel@noukod.com>
2019-01-22 15:00:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,24 @@
 class Clickbait < ActiveModel::Validator
-  def validate(title)
+  def validate(record)
     # if record.title
     messages = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-    messages.map do |msg|
+    messages.any do |msg|
+      record.title.include? msg
       unless title.include? msg # { |t| record.title.include? t }
         record.errors[:title] << 'the title does not contain the words'
       end
-     end
+    #  end
   end
 end
+
+# def validate(record)
+#     if record.title
+#       unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any? { |t| record.title.include? t }
+#         record.errors[:title] << 'the title does not contain the words'
+#       end
+#     end
+#   end
+# end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
